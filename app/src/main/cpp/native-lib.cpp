bd7b1b0d771b05d2d21fffca30eb34561e5ba8eb
tonyimax <lin_hong_quan_msn@hotmail.com>
2025-01-09 18:45:43 +0800

    增加JNI返回jintArray方法

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,14 @@ Java_com_example_usb_1auth_1demo_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+extern "C"
+JNIEXPORT jintArray JNICALL
+Java_com_example_usb_1auth_1demo_MainActivity_getIntArray(JNIEnv *env, jobject thiz) {
+    jintArray result = env->NewIntArray(2);
+    int temp[2];
+    temp[0]=1;
+    temp[1]=2;
+    env->SetIntArrayRegion(result,0,2,temp);
+    return result;
+}
--- app/src/main/java/com/example/usb_auth_demo/MainActivity.java
+++ app/src/main/java/com/example/usb_auth_demo/MainActivity.java
@@ -7,16 +7,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.usb_auth_demo.databinding.ActivityMainBinding;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
     private final String TAG = "usb_auth_demo";
+
     private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -43,8 +51,13 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }else{
                         Log.d(TAG, "设备拒绝授权: " + device);
+                        Toast.makeText(context,"设备拒绝授权",Toast.LENGTH_LONG).show();
                     }
                 }
+            }else if(UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)){
+                Toast.makeText(context,"USB已插入",Toast.LENGTH_LONG).show();
+            } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                Toast.makeText(context,"USB已拔出",Toast.LENGTH_LONG).show();
             }
         }
     };
@@ -59,6 +72,12 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        Button btnSave = findViewById(R.id.btnSaveToUDisk);
+        btnSave.setOnClickListener(view -> {
+            int[] arr =getIntArray();
+            System.out.printf("===> %d %d \n",arr[0],arr[1]);
+        });
 
         //获取USB管理服务
         UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
@@ -84,14 +103,28 @@ public class MainActivity extends AppCompatActivity {
                     new Intent(ACTION_USB_PERMISSION),
                     Build.VERSION.SDK_INT>=34? PendingIntent.FLAG_IMMUTABLE:PendingIntent.FLAG_MUTABLE);
             IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+            filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);//USB插入
+            filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);//USB拔出
             if (Build.VERSION.SDK_INT>=33){
                 registerReceiver(mUsbReceiver, filter,Context.RECEIVER_EXPORTED);
             }else{
                 registerReceiver(mUsbReceiver, filter);
             }
 
-            //请求USB设备权限
-            mUsbManager.requestPermission(dev, mPermissionIntent);
+            if (mUsbManager.hasPermission(dev)){
+                Toast.makeText(this,"已拥有USB设备权限",Toast.LENGTH_LONG).show();
+            }else{
+                //请求USB设备权限
+                mUsbManager.requestPermission(dev, mPermissionIntent);
+            }
+
+
+            //如果是USB配件，可以通信，如是U盘不行
+            /*UsbInterface intf = dev.getInterface(0);
+            UsbEndpoint endpoint = intf.getEndpoint(0);
+            UsbDeviceConnection connection = mUsbManager.openDevice(dev);
+            connection.claimInterface(intf, forceClaim);
+            connection.bulkTransfer(endpoint, bytes, bytes.length, TIMEOUT);*/
         }
 
 
@@ -102,4 +135,5 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native int[] getIntArray();
 }
